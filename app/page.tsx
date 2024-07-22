bae9e77dd5aac2e7b5f5869f71b50545cf1dce31
Geoffrey Sie <siegeoffrey@gmail.com>
2024-07-22 13:19:55 +0100

    Modified Sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import Image from "next/image";
-import Sidebar from "@/components/Sidebar";
+import SidebarMenu from "@/components/SidebarMenu";
+import { cn } from "@/lib/utils";
+import DppDashboard from "@/components/DppDashboard";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hydrogen Routes</h1>
+    <main className="">
+      <div
+      className={cn(
+        "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1 max-w-7xl mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden",
+        "h-screen"
+      )}
+      >
+        <SidebarMenu/>
+        <DppDashboard/>
+      </div>
     </main>
   );
 }
