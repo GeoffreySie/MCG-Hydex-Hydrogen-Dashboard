9ff8b5051dc397c0a38ef2b9e01ce0474370567e
Numan Ahmed <nahmedshaon@gmial.com>
2024-07-30 12:47:29 +0100

    SearchBar on the main page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import InfoWindow from '@/components/InformationWindow';
 import Map from '@/components/Map';
 import SidebarMenu from '@/components/SidebarMenu';
+import PassportList from '@/components/PassportList';
 
 const DashboardPage = () => {
   return (
@@ -10,8 +11,10 @@ const DashboardPage = () => {
     <div className='flex'>
       <InfoWindow />
     </div>
-      <div className='fixed h-full z-10'>
+      <div className='fixed h-full z-10 rounded-md flex flex-col md:flex-row w-max flex-1 mx-auto overflow-hidden",
+        "h-screen'>
       <SidebarMenu />
+      <PassportList />
       </div>
     <div className='w-screen h-screen'>
         <Map />
